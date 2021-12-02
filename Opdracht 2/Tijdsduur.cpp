b006d549bc26ccbc5d05abc2881bab6a216d1c04
Wouter de Bruijn <wouter@hedium.nl>
2021-12-02 19:37:27 +0100

    :sparkles: Added part of opdracht 2

---
+++ Opdracht 2/Tijdsduur.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "Tijdsduur.h"
+#include <iomanip>
+using namespace std;
+
+Tijdsduur::Tijdsduur(int minutes, int seconds)
+{
+  sec = seconds + minutes * 60;
+}
+
+Tijdsduur::Tijdsduur(int seconds)
+{
+  sec = seconds;
+}
+
+void Tijdsduur::print() const
+{
+  int minutes = this->sec / 60;
+  int seconds = this->sec - minutes * 60;
+
+  if (minutes > 0)
+  {
+    cout << minutes << " minutes and " << seconds << " seconds";
+  }
+  else
+  {
+    cout << "               " << seconds << " seconds";
+  }
+}
+
+void Tijdsduur::eraf(Tijdsduur t)
+{
+  sec -= t.sec;
+  if (sec < 0)
+    sec = 0;
+}
+
+void Tijdsduur::erbij(Tijdsduur t)
+{
+  sec += t.sec;
+}
+
+void Tijdsduur::maal(int times)
+{
+  sec *= times;
+}
