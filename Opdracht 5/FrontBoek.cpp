0e281dcd9a290da8d49b3373f4274f11eed9ea8e
Wouter de Bruijn <wouter@hedium.nl>
2022-01-18 20:56:51 +0100

    :poop: Added parts of assignment 5

---
+++ Opdracht 5/FrontBoek.cpp
@@ -0,0 +1,28 @@
+/*
+ * FrontBoek.cpp
+ *
+ *  Created on: Oct 31, 2017
+ *      Author: oop
+ */
+
+#include "FrontBoek.h"
+
+FrontBoek::FrontBoek(string n,string t):schrijver(n),titel(t) {
+
+}
+
+FrontBoek::~FrontBoek() {
+}
+
+string FrontBoek::naamSchrijver()const {
+	return schrijver;
+}
+string FrontBoek::naamTitel()const {
+	return titel;
+}
+void FrontBoek::verhoogSchrijver() {
+	schrijver +=1;
+}
+
+
+
---
+++ Opdracht 5/test1.cpp
@@ -0,0 +1,60 @@
+#include <CppUTest/CommandLineTestRunner.h>
+#include <CppUTest/TestHarness.h>
+
+using namespace std;
+#include <iostream>
+#include "FrontBoek.h"
+
+TEST_GROUP(FirstTestGroup)
+{
+    void setup() {
+        cout << "BeforeTest\n";
+    }
+
+    void teardown() {
+        cout << "AfterTest\n";
+    }
+};
+
+TEST_GROUP(boekengroep)
+{};
+
+TEST(boekengroep, front)
+{
+    FrontBoek boek("Wouter", "Logboek");
+
+    cout << "Schrijfer: '" << boek.naamSchrijver() << "'\n";
+
+    boek.verhoogSchrijver();
+
+    // Uhhh this doesn't work. Given functions tries to add 1 to a string???!
+
+    // If I wanted to test this, I would add something like:
+
+    /*
+        string oldSchrijfer = boek.naamSchrijver();
+        boek.verhoogSchrijver();
+        STRCMP_EQUAL(oldSchrijfer =+ 1, boek.naamSchrijfer());
+    */
+
+    cout << "Schrijfer: '" << boek.naamSchrijver() << "'\n";
+
+
+    FAIL("Fail me!");
+}
+
+TEST(FirstTestGroup, SecondTest)
+{
+    STRCMP_EQUAL("hello", "hello");
+}
+
+TEST(FirstTestGroup, ThirdTest)
+{
+    CHECK(true);
+}
+
+int main(int argc, char *argv[])
+{
+    CommandLineTestRunner::RunAllTests(argc, argv);
+    return 0;
+}
